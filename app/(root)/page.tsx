b3818dc6b8f98aad8afa9f50542f17497b391071
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2024-07-21 10:50:04 +0530

    update jsm-editor

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header'
+import { SignIn, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+    return (
+<main>Hii Praveen</main>
+    )
+}
+
+export default Home
