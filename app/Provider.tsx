4cf266510fe2f2250ff67dfe702ab5183cedd1d1
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2024-07-22 17:59:50 +0530

    adding active and update feature in title

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,16 +1,26 @@
 "use client";
+
 import { ReactNode } from "react";
 import {
   LiveblocksProvider,ClientSideSuspense} from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions';
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({children}:{children:ReactNode}) => {
+  const { user: clerkUser } = useUser();
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+    authEndpoint="/api/liveblocks-auth"
+    resolveUsers={async ({ userIds }) => {
+      const users = await getClerkUsers({ userIds});
+
+      return users;
+    }}
+    >
       <ClientSideSuspense fallback={<Loader/>}>
         {children}
       </ClientSideSuspense>
-
   </LiveblocksProvider>
   )
 }
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -22,7 +22,7 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses: []
+            defaultAccesses: ['room:write']
         });
         revalidatePath('/');
         // use when you return from serverside render
@@ -33,3 +33,34 @@ return parseStringify(room);
     }
 }
 
+export const getDocument = async ({ roomId, userId }: { roomId: string; userId: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+      
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+      
+        if(!hasAccess) {
+          throw new Error('You do not have access to this document');
+        }
+      
+        return parseStringify(room);
+    } catch (error) {
+      console.log(`Error happened while getting a room: ${error}`);
+    }
+  }
+
+  export const updateDocument = async (roomId: string, title: string) => {
+    try {
+      const updatedRoom = await liveblocks.updateRoom(roomId, {
+        metadata: {
+          title
+        }
+      })
+  
+      revalidatePath(`/documents/${roomId}`);
+  
+      return parseStringify(updatedRoom);
+    } catch (error) {
+      console.log(`Error happened while updating a room: ${error}`);
+    }
+  }
